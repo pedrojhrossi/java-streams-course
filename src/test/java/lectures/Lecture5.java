356909de7a88e2abb6a36d944a6e5dd2d2f8e5c7
Pedro Javier Hernández Rossi <47048314+pedrojhrossi@users.noreply.github.com>
2023-05-29 16:24:48 -0400

    Lecture 5 finished

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -8,6 +8,7 @@ import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import mockdata.MockData;
@@ -15,10 +16,17 @@ import org.junit.Test;
 
 public class Lecture5 {
 
+  final Predicate<Car> carPredicate = car -> car.getPrice() < 6500;
+
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
+    List<Car> carsFiltered = cars.stream()
+        .filter(carPredicate)
+        .collect(Collectors.toList());
 
+    carsFiltered.forEach(System.out::println);
+    System.out.println(carsFiltered.size());
   }
 
   @Test
@@ -26,12 +34,33 @@ public class Lecture5 {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
 
+    Function<Person, PersonDTO> personPersonDTOFunction = person -> new PersonDTO(person.getId(),
+        person.getFirstName(), person.getAge());
+
+    List<PersonDTO> dtos = people.stream()
+//        .map(person -> {
+//          PersonDTO dto = new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+//          return dto;
+//        })
+//        .map(personPersonDTOFunction)
+        .map(PersonDTO::map)
+        .collect(Collectors.toList());
+
+    dtos.forEach(System.out::println);
+    assertThat(dtos).hasSize(1000);
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
 
+    double average = MockData.getCars().stream()
+        .filter(car -> car.getPrice() < 1000)
+        .mapToDouble(Car::getPrice)
+        .average()
+        .orElse(0);
+
+    System.out.println(average);
   }
 
   @Test
