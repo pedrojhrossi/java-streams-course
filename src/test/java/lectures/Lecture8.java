fd32fe7c81f73b4574cc9fb29f877dbff4883b7b
Pedro Javier Hernández Rossi <47048314+pedrojhrossi@users.noreply.github.com>
2023-05-29 17:41:47 -0400

    Lecture 8 completed

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -16,7 +16,13 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
-
+    Map<String, List<Car>> grouping = MockData.getCars()
+        .stream()
+        .collect(Collectors.groupingBy(Car::getMake));
+    grouping.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
   }
 
   @Test
@@ -33,8 +39,11 @@ public class Lecture8 {
             "Alex",
             "Alex"
         );
-
-
+    Map<String, Long> counting = names.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    counting.forEach((name, count) -> {
+      System.out.println(name + " > " + count);
+    });
   }
 
 }
